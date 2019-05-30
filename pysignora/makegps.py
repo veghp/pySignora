2303585283a27052ccc61c3bf1210545f03bb248
veghp <31794642+veghp@users.noreply.github.com>
2019-05-30 11:42:33 +0100

    Copyright notice and license notice

--- pysignora/makegps.py
+++ pysignora/makegps.py
@@ -1,3 +1,21 @@
+# Copyright 2019 Peter Vegh
+# 
+# This file is part of pySignora.
+# 
+# pySignora is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# pySignora is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with pySignora.  If not, see <https://www.gnu.org/licenses/>.
+
+
 def makegps(repodf):
     # repodf is a pandas dataframe listing pathway-gene association with
     # 3 columns: pathway name/id, description, gene
