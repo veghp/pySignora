93d0179222a9a028b748b52874c5f7cc686d1cf1
Peter Vegh <pvegh@posteo.uk>
2021-04-03 13:51:03 +0100

    Format to Black

--- pysignora/makegps.py
+++ pysignora/makegps.py
@@ -1,17 +1,17 @@
 # Copyright 2019 Peter Vegh
-# 
+#
 # This file is part of pySignora.
-# 
+#
 # pySignora is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # pySignora is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with pySignora.  If not, see <https://www.gnu.org/licenses/>.
 
@@ -21,40 +21,40 @@ def makegps(repodf):
     # 3 columns: pathway name/id, description, gene
     import pandas as pd
 
-    repodf.columns = ['pwy', 'desc', 'gene']
+    repodf.columns = ["pwy", "desc", "gene"]
     repodf.drop_duplicates(inplace=True)
 
     # matrix format of bipartite graph:
     repomatrix = pd.crosstab(repodf.gene, repodf.pwy)
 
-    #pwy_degree = repomatrix.sum(axis=0)
-    #gene_degree = repomatrix.sum(axis=1)
-    #pug = gene_degree[gene_degree == 1] # pathway-unique genes
+    # pwy_degree = repomatrix.sum(axis=0)
+    # gene_degree = repomatrix.sum(axis=1)
+    # pug = gene_degree[gene_degree == 1] # pathway-unique genes
 
     # gene-gene graph. Edge weight is the number of common pathways:
     repotransform = (repomatrix).dot(repomatrix.T)
 
     # this section converts the matrix into a gene-gene-weight format
     edgelist_genes = repotransform.unstack()
-    edgelist_genes.index.names = ['g1', 'g2']
+    edgelist_genes.index.names = ["g1", "g2"]
     edgelist_genes = edgelist_genes.reset_index()
-    edgelist_genes.rename(columns={0:'weight'}, inplace=True)
-    edgelist_genes = (
-        edgelist_genes[edgelist_genes.weight != 0].reset_index(drop=True))
+    edgelist_genes.rename(columns={0: "weight"}, inplace=True)
+    edgelist_genes = edgelist_genes[edgelist_genes.weight != 0].reset_index(drop=True)
 
     # gene-pair signatures (gps) are gene-pairs specific to 1 pathway:
-    edgelist_genes_gp = edgelist_genes[edgelist_genes['weight'] == 1] 
+    edgelist_genes_gp = edgelist_genes[edgelist_genes["weight"] == 1]
     edgelist_genes_gp = edgelist_genes_gp.reset_index(drop=True)
 
     # assign pathway to each gps:
-    pwy_list = [None] * len(edgelist_genes_gp['g1'])
-    for index in range(0, len(pwy_list)): # range is [a, b[
-        g1 = edgelist_genes_gp['g1'][index]
-        g2 = edgelist_genes_gp['g2'][index]
-        pwy_list[index] = (
-            repomatrix.columns[repomatrix.loc[[g1, g2]].sum(axis=0) == 2][0])
+    pwy_list = [None] * len(edgelist_genes_gp["g1"])
+    for index in range(0, len(pwy_list)):  # range is [a, b[
+        g1 = edgelist_genes_gp["g1"][index]
+        g2 = edgelist_genes_gp["g2"][index]
+        pwy_list[index] = repomatrix.columns[repomatrix.loc[[g1, g2]].sum(axis=0) == 2][
+            0
+        ]
         # part ...[0] retrieves the string
 
-    edgelist_genes_gp['pwy'] = pwy_list
+    edgelist_genes_gp["pwy"] = pwy_list
 
     return edgelist_genes_gp
--- pysignora/signora.py
+++ pysignora/signora.py
@@ -1,17 +1,17 @@
 # Copyright 2019 Peter Vegh
-# 
+#
 # This file is part of pySignora.
-# 
+#
 # pySignora is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # pySignora is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with pySignora.  If not, see <https://www.gnu.org/licenses/>.
 
@@ -19,23 +19,24 @@
 def signora(genelist, gps):
     import pandas as pd
     import scipy.stats as stats
+
     # M is the total number of objects (all GPSs)
     # n is the total number of Type I objects (GPSs of pathway)
     # N is drawn without replacement from the population (GPSs of genelist)
     # x is the number of Type I objects in N (GPSs of pathway in genelist)
 
-    M = len(gps['g1']) # constant
+    M = len(gps["g1"])  # constant
 
-    genelist_which = gps['g1'].isin(genelist) & gps['g2'].isin(genelist)
+    genelist_which = gps["g1"].isin(genelist) & gps["g2"].isin(genelist)
     genelist_gps = gps[genelist_which]
 
-    N = len(genelist_gps['g1']) # constant
+    N = len(genelist_gps["g1"])  # constant
 
     # n_vector stores one n for each pathway:
-    n_vector = gps['pwy'].value_counts()
+    n_vector = gps["pwy"].value_counts()
 
-    repo_pw = n_vector.index.tolist() # gpsrepo pathways
-    genelist_pw = list(set(genelist_gps['pwy'])) # genelist-gpsrepo pathways
+    repo_pw = n_vector.index.tolist()  # gpsrepo pathways
+    genelist_pw = list(set(genelist_gps["pwy"]))  # genelist-gpsrepo pathways
 
     # x_vector stores one x for each pathway:
     x_vector = [None] * len(n_vector)
@@ -50,9 +51,11 @@ def signora(genelist, gps):
     ntests = len(pvalues)
     bonferroni = pvalues * float(ntests)
 
-    d = {'Pathway': pd.Series(repo_pw, index=repo_pw),
-         'Pvalue': pd.Series(pvalues, index=repo_pw),
-         'Pvalue_corr': pd.Series(bonferroni, index=repo_pw)}
+    d = {
+        "Pathway": pd.Series(repo_pw, index=repo_pw),
+        "Pvalue": pd.Series(pvalues, index=repo_pw),
+        "Pvalue_corr": pd.Series(bonferroni, index=repo_pw),
+    }
     results = pd.DataFrame(d)
 
     return results
